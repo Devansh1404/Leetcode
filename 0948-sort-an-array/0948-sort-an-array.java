2d121549aa1a44691fc585de0d449d0352d41cee
Devansh Shukla <devanshshukla1404@gmail.com>
2025-05-01 09:48:26 +0530

    Time: 25 ms (70.97%), Space: 56 MB (56.38%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,47 +1,43 @@
-import java.util.Random;
-
 class Solution {
-    Random rand = new Random(); // Reuse single Random instance
-
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
 
-    private void quickSort(int[] nums, int low, int high) {
-        if (low < high) {
-            int pivotIndex = randomizedPartition(nums, low, high);
-            quickSort(nums, low, pivotIndex - 1);
-            quickSort(nums, pivotIndex + 1, high);
+    private void mergeSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
         }
+        int mid = low + (high - low) / 2;
+        mergeSort(array, low, mid);
+        mergeSort(array, mid + 1, high);
+        merge(array, low, mid, high);
     }
 
-    private int randomizedPartition(int[] nums, int low, int high) {
-        int pivotIdx = low + rand.nextInt(high - low + 1);
-        swap(nums, pivotIdx, high); // Swap random pivot with last
-        return partition(nums, low, high);
-    }
+    private void merge(int[] array, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+        int[] leftPart = new int[n1];
+        int[] rightPart = new int[n2];
 
-    private int partition(int[] nums, int low, int high) {
-        int pivot = nums[high];
-        int i = low - 1;
+        System.arraycopy(array, low, leftPart, 0, n1);
+        System.arraycopy(array, mid + 1, rightPart, 0, n2);
 
-        for (int j = low; j < high; j++) {
-            if (nums[j] <= pivot) {
-                i++;
-                swap(nums, i, j);
+        int p1 = 0, p2 = 0, writeInd = low;
+        while (p1 < n1 && p2 < n2) {
+            if (leftPart[p1] <= rightPart[p2]) {
+                array[writeInd++] = leftPart[p1++];
+            } else {
+                array[writeInd++] = rightPart[p2++];
             }
         }
 
-        swap(nums, i + 1, high);
-        return i + 1;
-    }
+        while (p1 < n1) {
+            array[writeInd++] = leftPart[p1++];
+        }
 
-    private void swap(int[] nums, int i, int j) {
-        if (i != j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+        while (p2 < n2) {
+            array[writeInd++] = rightPart[p2++];
         }
     }
 }
